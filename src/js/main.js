472ff30d5236050cae255e88e67f6fc886beb4d5
sergeyFrontend <utemishevsergey02@gmail.com>
2022-11-07 22:18:32 +0500

    add menu burger

--- src/js/main.js
+++ src/js/main.js
@@ -12,3 +12,12 @@ setInterval(function(){
   preloader.classList.add("hide-preloader")
 },2)
 }
+
+
+
+const gb = document.querySelector('.header__burger');
+const header = document.querySelector('.header');
+
+gb.addEventListener('click',()=>{
+header.classList.toggle('active');
+})
