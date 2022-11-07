e9f6c3985a6ec20a7737bfb84b1568d535077488
sergeyFrontend <utemishevsergey02@gmail.com>
2022-11-07 14:43:59 +0500

    ref:add js preloader

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,13 @@ import './_vendor';
 import vars from './_vars';
 import './_functions';
 import './_components';
+
+
+
+window.onload = function(){
+let preloader = document.getElementById("preloader");
+preloader.classList.add("hide-preloader")
+setInterval(function(){
+  preloader.classList.add("hide-preloader")
+},2)
+}
